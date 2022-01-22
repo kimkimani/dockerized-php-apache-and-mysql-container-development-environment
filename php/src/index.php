b4abf099809f7f379129cfe283968150d147fc89
kimkimani <kimkimani016@gmail.com>
2022-01-22 18:07:09 +0300

    dockerized-php-apache-and-mysql-container-development-environment

---
+++ php/src/index.php
@@ -0,0 +1,37 @@
+<?php
+//These are the defined authentication environment in the db service
+
+// The MySQL service named in the docker-compose.yml.
+$host = 'db';
+
+// Database use name
+$user = 'MYSQL_USER';
+
+//database user password
+$pass = 'MYSQL_PASSWORD';
+
+// check the MySQL connection status
+$conn = new mysqli($host, $user, $pass, $mydatabase);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected to MySQL server successfully!";
+}
+
+
+// select query
+$sql = 'SELECT * FROM users';
+
+if ($result = $conn->query($sql)) {
+    while ($data = $result->fetch_object()) {
+        $users[] = $data;
+    }
+}
+
+foreach ($users as $user) {
+    echo "<br>";
+    echo $user->username . " " . $user->password;
+    echo "<br>";
+}
+?>
